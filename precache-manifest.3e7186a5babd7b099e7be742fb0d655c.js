570995b59d27f8f7396348e4756ff3705f53c4ad
Adarsh Mishra <adarshkaushik01@gmail.com>
2021-03-05 22:07:27 +0530

    Updates

---
+++ precache-manifest.3e7186a5babd7b099e7be742fb0d655c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1c21507511350c902cc07b6c1f54e1",
+    "url": "/usurvey/index.html"
+  },
+  {
+    "revision": "8a04c58028648d57fd9c",
+    "url": "/usurvey/static/css/main.dc6b257c.chunk.css"
+  },
+  {
+    "revision": "fae3068c2f276c8a47dd",
+    "url": "/usurvey/static/js/2.1c4faac2.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/usurvey/static/js/2.1c4faac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a04c58028648d57fd9c",
+    "url": "/usurvey/static/js/main.65cbf56a.chunk.js"
+  },
+  {
+    "revision": "1189a783db53ad18ed33",
+    "url": "/usurvey/static/js/runtime-main.d8ad7bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/usurvey/static/media/logo.5d5d9eef.svg"
+  }
+]);
